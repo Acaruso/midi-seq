4aa536bf5298e5ae9da791559bccda453910606e
Acaruso <anthonycaruso102@gmail.com>
2024-01-19 06:33:08 -0500

    chord voice leading

--- midi-seq/src/midi/chord_gen_voice_leading.hpp
+++ midi-seq/src/midi/chord_gen_voice_leading.hpp
@@ -4,41 +4,30 @@
 
 #include "chords.hpp"
 
-struct VChord {
+struct MChord {
     int degree;
     int lowestNote;
     ChordType chordType;
     ChordInversion chordInversion;
 };
 
-// enum ChordType {
-//     MAJOR,
-//     MINOR,
-// };
-
-// enum ChordInversion {
-//     ROOT,
-//     FIRST_INV,
-//     SECOND_INV,
-// };
-
-static std::vector chords = {
-    VChord{1, 1, MAJOR, ROOT},
-    VChord{6, 1, MINOR, ROOT},
-    VChord{4, 1, MAJOR, SECOND_INV},
-    VChord{2, 2, MINOR, ROOT},
-    VChord{5, 2, MAJOR, SECOND_INV},
-    VChord{3, 3, MINOR, ROOT},
-    VChord{1, 3, MAJOR, FIRST_INV},
-    VChord{6, 3, MINOR, SECOND_INV},
-    VChord{4, 4, MAJOR, ROOT},
-    VChord{2, 4, MINOR, FIRST_INV},
-    VChord{5, 5, MAJOR, ROOT},
-    VChord{3, 5, MINOR, FIRST_INV},
-    VChord{1, 5, MAJOR, SECOND_INV},
-    VChord{6, 6, MINOR, ROOT},
-    VChord{4, 6, MAJOR, FIRST_INV},
-    VChord{2, 6, MINOR, SECOND_INV},
-    VChord{5, 7, MAJOR, FIRST_INV},
-    VChord{3, 7, MINOR, SECOND_INV},
+static std::vector<MChord> chords = {
+    MChord{1, 1, MAJOR, ROOT},
+    MChord{6, 1, MINOR, ROOT},
+    MChord{4, 1, MAJOR, SECOND_INV},
+    MChord{2, 2, MINOR, ROOT},
+    MChord{5, 2, MAJOR, SECOND_INV},
+    MChord{3, 3, MINOR, ROOT},
+    MChord{1, 3, MAJOR, FIRST_INV},
+    MChord{6, 3, MINOR, SECOND_INV},
+    MChord{4, 4, MAJOR, ROOT},
+    MChord{2, 4, MINOR, FIRST_INV},
+    MChord{5, 5, MAJOR, ROOT},
+    MChord{3, 5, MINOR, FIRST_INV},
+    MChord{1, 5, MAJOR, SECOND_INV},
+    MChord{6, 6, MINOR, ROOT},
+    MChord{4, 6, MAJOR, FIRST_INV},
+    MChord{2, 6, MINOR, SECOND_INV},
+    MChord{5, 7, MAJOR, FIRST_INV},
+    MChord{3, 7, MINOR, SECOND_INV},
 };
